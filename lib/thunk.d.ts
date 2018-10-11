8746c2fc416fdb6aaf4fd596bda6d282ff6efce5
KostNazvanov <nazvanov.Kst@gmail.com>
2018-10-11 15:52:49 +0300

    Add custom layout for detail/edit page

--- lib/thunk.d.ts
+++ lib/thunk.d.ts
@@ -1,2 +1,4 @@
-export declare type Thunk<T> = T | (() => T);
+import { RouteComponentProps } from 'react-router';
+import { Entity } from './model/Entity';
+export declare type Thunk<T> = T | ((entity?: Entity<any>, route?: RouteComponentProps) => T);
 export declare function getThunkValue<T>(thunk: Thunk<T>): T;
