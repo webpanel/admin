4c13bcbd022277b6f67e1e57cce71f27a76be9b6
KostNazvanov <nazvanov.Kst@gmail.com>
2018-10-15 13:23:49 +0300

    ColorPicker entity field

---
+++ lib/model/fields/EntityFieldColor.d.ts
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { EntityField } from '../EntityField';
+export declare class EntityFieldColor<T, C> extends EntityField<T, C> {
+    inputElement(props?: {
+        value?: any;
+        onChange?: (value: any) => void;
+        autoFocus?: boolean;
+    }): React.ReactNode;
+}
