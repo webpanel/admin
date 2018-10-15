e8e35816a039b4c8361f2b2dbebd9bd82afcf4e7
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2018-10-15 23:37:08 +0200

    Update color field rendering

--- lib/model/fields/EntityFieldColor.d.ts
+++ lib/model/fields/EntityFieldColor.d.ts
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { EntityField } from '../EntityField';
 export declare class EntityFieldColor<T, C> extends EntityField<T, C> {
+    readonly render: ((record: T) => React.ReactNode);
     inputElement(props?: {
         value?: any;
         onChange?: (value: any) => void;
