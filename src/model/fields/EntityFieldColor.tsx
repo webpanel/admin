29ffbee6ba01e5c686cfb8fabf677e667c2d85cb
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2018-10-15 23:37:08 +0200

    Update color field rendering

--- src/model/fields/EntityFieldColor.tsx
+++ src/model/fields/EntityFieldColor.tsx
@@ -2,16 +2,29 @@ import * as React from 'react';
 import { EntityField } from '../EntityField';
 
 export class EntityFieldColor<T, C> extends EntityField<T, C> {
+  public get render(): ((record: T) => React.ReactNode) {
+    return (values: any) => {
+      const color = values[this.name];
+      return color ? (
+        <div
+          style={{
+            width: '30px',
+            height: '20px',
+            backgroundColor: color,
+            borderRadius: '3px'
+          }}
+        />
+      ) : (
+        '–'
+      );
+    };
+  }
+
   public inputElement(props?: {
     value?: any;
     onChange?: (value: any) => void;
     autoFocus?: boolean;
   }): React.ReactNode {
-    return (
-      <input
-        type="color"
-        {...props}
-      />
-    );
+    return <input type="color" {...props} />;
   }
 }
