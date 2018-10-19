1a963ae78aff814dcb7e8be8cf2d189d5a3556d9
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2018-10-19 12:05:58 +0200

    Switch to ts-thunk package

--- src/thunk.tsx
+++
@@ -1,8 +0,0 @@
-export type Thunk<T> = T | ((...args: any[]) => T);
-
-export function getThunkValue<T>(thunk: Thunk<T>): T {
-  if (typeof thunk === 'function') {
-    return thunk();
-  }
-  return thunk;
-}
