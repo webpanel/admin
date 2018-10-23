77d7e578d86ea8824e6db948a56d9f51074c1c58
KostNazvanov <nazvanov.Kst@gmail.com>
2018-10-23 13:48:05 +0300

    Add entity.enumField

---
+++ lib/model/fields/EntityFieldEnum.d.ts
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { EntityField, IEntityFieldConfig } from '../EntityField';
+import { Thunk } from 'ts-thunk';
+export interface IOption {
+    value: string;
+    label: string;
+}
+export interface IEntityFieldEnumConfig<T> extends IEntityFieldConfig<T> {
+    options: Thunk<IOption[]>;
+}
+export declare class EntityFieldEnum<T> extends EntityField<T, IEntityFieldEnumConfig<T>> {
+    inputElement(props?: {
+        value?: any;
+        onChange?: (value: any) => void;
+        autoFocus?: boolean;
+    }): React.ReactNode;
+}
