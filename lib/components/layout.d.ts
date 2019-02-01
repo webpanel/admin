4b0add268bc99288b64659f2af36c99cb57058e9
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2019-02-01 09:59:31 +0100

    Update antd layouts

--- lib/components/layout.d.ts
+++ lib/components/layout.d.ts
@@ -3,11 +3,13 @@ import { Entity } from '../model/Entity';
 import { LayoutProps } from 'webpanel-antd/lib/layout/Layout';
 import { Thunk } from 'ts-thunk';
 import { HeaderConfig } from 'webpanel-antd/lib/layout/Header';
+import { MenuConfig } from 'webpanel-antd/lib/layout/Menu';
 export interface ILayoutProps {
     entities?: Thunk<Entity<any>[]>;
     menuItems?: Thunk<React.ReactNode[]>;
     structureItems?: Thunk<React.ReactNode[]>;
     header?: HeaderConfig;
+    menu?: MenuConfig;
 }
 export declare class AdminLayout extends React.Component<ILayoutProps & LayoutProps> {
     render(): JSX.Element;
