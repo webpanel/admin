487858f0560e8a08c677f17af2cf9264e8fa4ce8
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2019-07-11 22:23:21 +0200

    Add EntityAddButton to control creation flow

---
+++ lib/components/buttons/EntityAddButton.d.ts
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { Entity } from '../../model/Entity';
+export interface IEntityAddButtonProps {
+    flow: 'modal' | 'redirect';
+    initialValues?: {
+        [key: string]: any;
+    };
+}
+interface IEntityAddButtonComponentProps extends IEntityAddButtonProps {
+    entity: Entity<any>;
+    onCreate?: () => void;
+}
+interface IEntityAddButtonState {
+    showModal: boolean;
+}
+export declare class EntityAddButton extends React.Component<IEntityAddButtonComponentProps, IEntityAddButtonState> {
+    state: {
+        showModal: boolean;
+    };
+    render(): JSX.Element;
+    private hideModal;
+}
+export {};
