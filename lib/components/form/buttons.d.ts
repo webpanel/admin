7db546f9b493a602e353a4089c2bb841b9ddbb43
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2019-10-11 11:17:19 +0200

    Disable multiple save options

--- lib/components/form/buttons.d.ts
+++ lib/components/form/buttons.d.ts
@@ -2,7 +2,6 @@ import * as React from 'react';
 export declare type SaveOption = 'edit' | 'add';
 export interface ResourceFormButtonsProps {
     hasChanges: boolean;
-    handleSave: (option: SaveOption) => void;
     handleReset: () => void;
 }
 export declare class ResourceFormPageButtons extends React.Component<ResourceFormButtonsProps> {
