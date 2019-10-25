e1289375011b093db9506457862f3a043a00b75a
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2019-10-25 15:57:48 +0200

    Update webpanel-data to 0.0.59

--- lib/model/fields/EntityFieldString copy.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { EntityField, IEntityFieldConfig } from '../EntityField';
-export declare class EntityFieldString<T, C extends IEntityFieldConfig<T>> extends EntityField<T, C> {
-    inputElement(props?: {
-        value?: any;
-        onChange?: (value: any, valueElement: React.ReactNode) => void;
-        autoFocus?: boolean;
-    }): React.ReactNode;
-}
