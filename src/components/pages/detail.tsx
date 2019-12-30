ba85727d3cb5b73a6dee8ef63f9e4b169ccf6c87
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2019-12-30 14:32:13 +0100

    Update detail description fields

--- src/components/pages/detail.tsx
+++ src/components/pages/detail.tsx
@@ -9,12 +9,23 @@ import { Thunk, resolveOptionalThunk } from "ts-thunk";
 
 import { DescriptionsProps } from "antd/lib/descriptions";
 import { Entity } from "../../model/Entity";
+import { EntityField } from "../../model/EntityField";
 // import { Link  } from 'react-router-dom';
 import { TFunction } from "i18next";
 import { Translation } from "react-i18next";
 
+export interface IEntityDetailFieldOptions {
+  field: string | null;
+  span?: number;
+}
+
+export type IEntityDetailConfigField =
+  | IEntityDetailFieldOptions
+  | string
+  | null;
+
 export interface IEntityDetailConfig {
-  fields?: Thunk<string[]>;
+  fields?: Thunk<IEntityDetailConfigField[]>;
   pollInterval?: number;
   wrapperType?: "card" | "plain" | "modal";
   modal?: ModalProps;
@@ -36,10 +47,30 @@ export class EntityDetail extends React.Component<IEntityDetailProps> {
       modal
     } = this.props;
 
-    let entityFields = entity.detailFields;
+    let entityFields: EntityField<any, any>[] = entity.detailFields;
+    let descriptionItems: {
+      field: EntityField<any, any> | null;
+      span?: number;
+    }[] = entityFields.map(field => ({
+      field
+    }));
     const _fields = resolveOptionalThunk(fields);
     if (typeof _fields !== "undefined") {
-      entityFields = _fields.map(name => entity.getFieldOrFail(name));
+      const fs: IEntityDetailFieldOptions[] = _fields.map(f => {
+        let _f: IEntityDetailFieldOptions = { field: null };
+        if (typeof f === "string" || f === null) {
+          return { field: f };
+        } else {
+          _f = f;
+        }
+        return _f;
+      });
+      descriptionItems = fs.map(f => {
+        return {
+          field: (f.field && entity.getFieldOrFail(f.field)) || null,
+          span: f.span
+        };
+      });
     }
 
     const contentFn = (resource: Resource, t: TFunction) => (
@@ -49,14 +80,20 @@ export class EntityDetail extends React.Component<IEntityDetailProps> {
         column={{ md: 2, xs: 1 }}
         {...this.props.desriptions}
       >
-        {entityFields.map((field, i) => (
+        {descriptionItems.map((item, i) => (
           <Descriptions.Item
-            key={`${field.name}_${i}`}
-            label={t(`${entity.name}.${field.name}`, {
-              defaultValue: field.title
-            })}
+            span={item.span}
+            key={`${(item.field && item.field.name) || "empty"}_${i}`}
+            label={
+              item.field &&
+              t(`${entity.name}.${item.field.name}`, {
+                defaultValue: item.field.title
+              })
+            }
           >
-            {(resource.data && field.render(resource.data)) || "–"}
+            {item.field
+              ? (resource.data && item.field.render(resource.data)) || "–"
+              : ""}
           </Descriptions.Item>
         ))}
       </Descriptions>
@@ -71,8 +108,8 @@ export class EntityDetail extends React.Component<IEntityDetailProps> {
             dataSource={entity.dataSource}
             fields={[
               "id",
-              ...(entityFields
-                .map(x => x.fetchField())
+              ...(descriptionItems
+                .map(x => x.field && x.field.fetchField())
                 .filter(x => x) as string[])
             ]}
             pollInterval={pollInterval}
