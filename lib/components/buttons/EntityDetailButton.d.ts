161ee4714c46c44dc703821b8e36e2071ccd343d
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2020-01-03 16:21:49 +0100

    Add hideLinkToDetail to entityDetailButton

--- lib/components/buttons/EntityDetailButton.d.ts
+++ lib/components/buttons/EntityDetailButton.d.ts
@@ -1,18 +1,19 @@
-import * as React from 'react';
-import { ButtonProps } from 'antd/lib/button';
-import { Entity } from '../../model/Entity';
-import { IEntityDetailConfig } from '../pages/detail';
-import { ModalProps } from 'antd/lib/modal';
-import { ResourceID } from 'webpanel-data';
+import * as React from "react";
+import { ButtonProps } from "antd/lib/button";
+import { Entity } from "../../model/Entity";
+import { IEntityDetailConfig } from "../pages/detail";
+import { ModalProps } from "antd/lib/modal";
+import { ResourceID } from "webpanel-data";
 export interface IEntityDetailButtonModalFlow {
-    type: 'modal';
+    type: "modal";
     modal?: ModalProps;
 }
-declare type FlowType = 'redirect' | IEntityDetailButtonModalFlow;
+declare type FlowType = "redirect" | IEntityDetailButtonModalFlow;
 export interface DetailEntityProps extends IEntityDetailConfig {
     flow?: FlowType;
     key?: string;
     button?: ButtonProps;
+    hideLinkToDetail?: boolean;
 }
 export interface DetailEntityButtonProps extends DetailEntityProps {
     entity: Entity;
--- src/components/buttons/EntityDetailButton.tsx
+++ src/components/buttons/EntityDetailButton.tsx
@@ -1,24 +1,25 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { Button } from 'antd';
-import { ButtonProps } from 'antd/lib/button';
-import { Entity } from '../../model/Entity';
-import { IEntityDetailConfig } from '../pages/detail';
-import { Link } from 'webpanel-antd';
-import { ModalProps } from 'antd/lib/modal';
-import { ResourceID } from 'webpanel-data';
-import { Translation } from 'react-i18next';
+import { Button } from "antd";
+import { ButtonProps } from "antd/lib/button";
+import { Entity } from "../../model/Entity";
+import { IEntityDetailConfig } from "../pages/detail";
+import { Link } from "webpanel-antd";
+import { ModalProps } from "antd/lib/modal";
+import { ResourceID } from "webpanel-data";
+import { Translation } from "react-i18next";
 
 export interface IEntityDetailButtonModalFlow {
-  type: 'modal';
+  type: "modal";
   modal?: ModalProps;
 }
-type FlowType = 'redirect' | IEntityDetailButtonModalFlow;
+type FlowType = "redirect" | IEntityDetailButtonModalFlow;
 
 export interface DetailEntityProps extends IEntityDetailConfig {
   flow?: FlowType;
   key?: string;
   button?: ButtonProps;
+  hideLinkToDetail?: boolean;
 }
 
 export interface DetailEntityButtonProps extends DetailEntityProps {
@@ -32,11 +33,20 @@ export class DetailEntityButton extends React.Component<
   public state = { showModal: false };
 
   public render() {
-    const { entity, entityId, flow, key, button, modal, ...rest } = this.props;
+    const {
+      entity,
+      entityId,
+      flow,
+      key,
+      button,
+      modal,
+      hideLinkToDetail,
+      ...rest
+    } = this.props;
 
-    let _flow = flow || 'redirect';
+    let _flow = flow || "redirect";
 
-    if (_flow === 'redirect') {
+    if (_flow === "redirect") {
       return (
         <Link to={entity.getDetailLink(entityId)} key={key}>
           <Button htmlType="button" icon="search" {...button} />
@@ -44,25 +54,27 @@ export class DetailEntityButton extends React.Component<
       );
     }
 
-    if (_flow.type === 'modal') {
+    if (_flow.type === "modal") {
       const modal = _flow.modal;
       return (
         <Translation key={key}>
           {t => (
             <>
-              <div style={{ position: 'absolute' }}>
+              <div style={{ position: "absolute" }}>
                 {entity.getDetailView(entityId, {
-                  wrapperType: 'modal',
+                  wrapperType: "modal",
                   modal: {
                     title: t(`${entity.name}._title`, {
                       defaultValue: `${entity.title}`
                     }),
                     onCancel: this.hideModal,
-                    footer: entity.getDetailButton(entityId, {
-                      flow: 'redirect',
-                      button: { icon: 'select' }
-                    }),
-                    width: '80%',
+                    footer:
+                      !hideLinkToDetail &&
+                      entity.getDetailButton(entityId, {
+                        flow: "redirect",
+                        button: { icon: "select" }
+                      }),
+                    width: "80%",
                     ...modal,
                     visible: this.state.showModal,
                     destroyOnClose: true
