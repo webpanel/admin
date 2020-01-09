c6cbdddb1fd234866d25e27bce042f09110e20fb
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2020-01-09 02:11:26 +0100

    Add entity picker

---
+++ lib/components/entity-picker/index.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="react" />
+import { Entity } from "../../model/Entity";
+import { SelectProps } from "antd/lib/select";
+export interface EntitySelectConfig extends SelectProps {
+    key?: string;
+}
+export interface EntitySelectProps extends EntitySelectConfig {
+    entity: Entity;
+}
+export declare const EntitySelect: (props: EntitySelectProps) => JSX.Element;
