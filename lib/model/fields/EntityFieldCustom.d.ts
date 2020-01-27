dc58f6d393ec29d09f11844201e76ce90bfb8950
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2020-01-27 10:16:42 +0100

    Add fetchField to customField

--- lib/model/fields/EntityFieldCustom.d.ts
+++ lib/model/fields/EntityFieldCustom.d.ts
@@ -1,12 +1,14 @@
-import * as React from 'react';
-import { EntityField, IEntityFieldConfig } from '../EntityField';
+import * as React from "react";
+import { EntityField, IEntityFieldConfig } from "../EntityField";
 export interface IEntityFieldCustomConfig<T> extends IEntityFieldConfig<T> {
+    fetchField?: string;
     inputElement: (props: {
         value: T | undefined;
         onChange: (value?: T) => void;
     }) => React.ReactNode;
 }
 export declare class EntityFieldCustom<T, C extends IEntityFieldCustomConfig<T>> extends EntityField<T, C> {
+    fetchField(): string | null;
     inputElement(props?: {
         value?: T;
         onChange?: (value: any, valueElement: React.ReactNode) => void;
